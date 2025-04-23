2132b109c4d2923707181b273dbef130235710d3
nithinramesh <nithinramesh67@gmail.com>
2025-04-23 14:58:03 +0530

    Async and Await Importance (S93 and S94)

--- YumBlazor/Repository/IRepository/ICategoryRepository.cs
+++ YumBlazor/Repository/IRepository/ICategoryRepository.cs
@@ -1,13 +1,14 @@
 ﻿using YumBlazor.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace YumBlazor.Repository.IRepository
 {
 	public interface ICategoryRepository
 	{
-		public Category Create(Category obj);
-		public Category Update(Category obj);
-		public bool Delete(int id);
-		public Category Get(int id);
-		public IEnumerable<Category> GetAll();
+		public Task<Category> CreateAsync(Category obj);
+		public Task<Category> UpdateAsync(Category obj);
+		public Task<bool> DeleteAsync(int id);
+		public Task<Category> GetAsync(int id);
+		public Task<IEnumerable<Category>> GetAllAsync();
 	}
 }
