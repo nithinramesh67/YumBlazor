d16ff78ab9188d016330e13c43cfc1ddb7ff1b1c
nithinramesh <nithinramesh67@gmail.com>
2025-04-25 02:20:10 +0530

    101. Show Modal from Delete Button

--- YumBlazor/wwwroot/js/common.js
+++ YumBlazor/wwwroot/js/common.js
@@ -1,6 +1,6 @@
 ﻿function ShowConfirmationModal() {
-    bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')).show();
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('bsConfirmationModal')).show();
 }
 function HideConfirmationModal() {
-    bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal')).hide();
+    bootstrap.Modal.getOrCreateInstance(document.getElementById('bsConfirmationModal')).hide();
 }
